1d1a27ae320111cffd1277123f3b490bdb7521e7
nary lozano <narylozano@gmail.com>
2023-05-17 21:31:38 +0200

    Add test middleware

--- src/types/customError.ts
+++ src/types/customError.ts
@@ -1,5 +1,9 @@
 class CustomError extends Error {
-  constructor(public statusCode: number, message: string) {
+  constructor(
+    public statusCode: number,
+    message: string,
+    public publicMessage: string
+  ) {
     super(message);
     this.statusCode = statusCode;
   }
